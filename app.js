a37a44a5171c161edd8eebe9f03c7abae680d841
Mikahel Gonçalves <mikahel800@gmail.com>
2024-06-05 12:39:26 -0300

    Design inicial.

---
+++ app.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('[data-js="form"]')
+const input = document.querySelector('[data-js="form-control"]')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    
+})
